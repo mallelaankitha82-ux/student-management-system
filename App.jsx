766aa310a242100c727c9720de44c4904607d1f1
mallelaankitha82-ux <mallelaankitha82@gmail.com>
2026-04-30 15:41:31 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+//import "./App.css";
+//import Header from "./components/Header";
+import "./App.css";
+import Header from "./components/Header";
+import StudentList from "./components/StudentList";
+
+export default function App() {
+  return (
+    <div className="sma-app">
+      <Header />
+      <main className="sma-main">
+        <StudentList />
+      </main>
+    </div>
+  );
+}
